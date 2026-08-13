0f485cac60779edcc63d00c1c2278f9f78ea557b
霧流羽kiriha <uraume2027@gmail.com>
2026-08-13 09:44:37 +0800

    Update Processing errors.py

--- Processing errors.py
+++ Processing errors.py
@@ -1,20 +1,25 @@
 # if there's an error it will be ignored(no output)
 try:
-    a,b=map(int,input().split())
-    print(a//b)
+    a, b =map(int,input().split())
+    print(a // b)
 except EOFError:
     pass
 
 
-#if an error occurs,for example dividing by zero
-a,b=map(int,input().split())
-print(a//b)
-#terminal will show:
-#ZeroDivisionError: integer division or modulo by zero
+a,b = map(int,input().split())
+print(a // b)
+"""
+In the code above:
+If b is 0, a ZeroDivisionError will occur because
+division by zero is not allowed.
+
+The terminal will display:
+ZeroDivisionError: integer division or modulo by zero
+"""
 
 
-#using raise to report error by selves
-a,b=map(int,input().split())
+# using raise to report error by selves
+a,b = map(int,input().split())
 print(a//b)
 raise ZeroDivisionError("Hah ha u the fool😏")
 
@@ -22,5 +27,5 @@ raise ZeroDivisionError("Hah ha u the fool😏")
 try:
     a,b=map(int,input().split())
     print(a//b)
-except Exception as e: # It can be used in try/except blocks to catch problems in the program
+except Exception as e:  # It can be used in try/except blocks to catch problems in the program
     print(f"the {str(e)} error is appeared,fuck off!")

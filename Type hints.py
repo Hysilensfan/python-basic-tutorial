dc8f40288dc4b70411ec6f75836cfc0f312da0ae
霧流羽kiriha <uraume2027@gmail.com>
2026-08-13 10:42:02 +0800

    Create Type hints.py

---
+++ Type hints.py
@@ -0,0 +1,10 @@
+"""
+Type hints:
+To labelling an attribute that a statement would return.
+"""
+string_: str = input()  # We hope get a variable, and this variable's class is string
+decimal_: int = int(input())  # We hope get a variable, and this variable's class is integer
+doulbe_: float = float(input())  # We hope get a variable, and this variable's class is float
+dyamic_list: list[str] = list(input().split())  # We hope get a variable, and this variable's class is list
+sets_: set[str] = set(input().split())  # We hope get a variable, and this variable's class is set
+dict_: dict = dict(input().split())  # We hope get a variable, and this variable's class is dictionary
